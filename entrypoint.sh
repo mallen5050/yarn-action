da945503d5ce2b3a5c282c9b07513d270c7f7b02
mallen5050 <32575828+mallen5050@users.noreply.github.com>
2021-09-30 12:12:41 -0400

    add apt commands to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,16 @@ set -e
 
 env
 rm -f ./.npmrc
+apk add --update --no-cache gcc g++ make libc6-compat \
+apk add vips-dev fftw-dev build-base --no-cache --allow-untrusted --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --repository http://dl-3.alpinelinux.org/alpine/edge/main \
 
+    apk add --update --no-cache \
+      --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ \
+#        fftw-dev gcc g++ make libc6-compat \
+        python \
+        python-dev \
+        py-pip \
+        build-base \
+        vips-dev \
 
 sh -c "yarn $*"
